a18e913cb64fe7b516324773a16d77322a151150
Tushar-W-QA <tusharwarad2929@gmail.com>
2024-09-12 09:51:22 +0530

    test

---
+++ src/sept/ex_12092024/Lab01.java
@@ -0,0 +1,13 @@
+package sept.ex_12092024;
+
+public class Lab01 {
+    public static void main(String[] args) {
+        // datatype and variables
+        // primitive have two type datatype
+        // 1. Boolean --> can have only true and false value
+
+        boolean Is_water_solid_or_liquid = true;
+        System.out.println(Is_water_solid_or_liquid);
+
+    }
+}
