276403aa84d777ed05e60e0ae459788d1614cdbb
Tushar-W-QA <tusharwarad2929@gmail.com>
2024-09-13 08:36:58 +0530

    add to file

--- src/sept/ex_12092024/Lab01_booleanDT.java
+++ src/sept/ex_12092024/Lab01_booleanDT.java
@@ -2,10 +2,11 @@ package sept.ex_12092024;
 
 public class Lab01_booleanDT {
     public static void main(String[] args) {
-        // datatype and variables
-        // primitive have two type datatype
-        // 1. Boolean --> can have only true and false value
-
+        // datatype and variables (Java is caseSensitive language)
+        // primitive have two type datatype (
+        // 1. Boolean --> can have only true and false value (False or false both are different in java)
+        // write code formate
+    //  datatype variable_name = variable_value
         boolean is_water_solid = false;
         boolean is_water_liquid = true;
         System.out.println(is_water_solid);
---
+++ src/sept/ex_12092024/Lab02_Variable_name.java
@@ -0,0 +1,11 @@
+package sept.ex_12092024;
+
+public class Lab02_Variable_name {
+    public static void main(String[] args) {
+        // variable name (Identifier) --> Need to follow some rules
+        // Identifier can contain Alphabets, Digits, and two special symbol i.e. Dollar ($) & Underscore (_).
+        // First character of an identifier should be an Alphabet or Dollar ($) or Underscore (_).
+        // Keywords or Reserved words can't be used as Identifiers.
+
+    }
+}
