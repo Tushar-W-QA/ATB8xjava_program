e739a21c2db12db013411cd6cabb269e9bba8eda
Tushar-W-QA <tusharwarad2929@gmail.com>
2024-09-14 07:38:51 +0530

    Lab02

--- src/sept/ex_12092024/Lab02_Variable_name.java
+++ src/sept/ex_12092024/Lab02_Variable_name.java
@@ -7,5 +7,20 @@ public class Lab02_Variable_name {
         // First character of an identifier should be an Alphabet or Dollar ($) or Underscore (_).
         // Keywords or Reserved words can't be used as Identifiers.
 
+    //  boolean 1233 = true; // not correct format
+        boolean t = false;
+        boolean $ = true;
+        boolean _123 = true;
+
+        boolean tushar$warad = true;
+        boolean tushar_warad = true;
+        boolean $rj = false;
+    //  boolean public = true;  // public  is keyword so, we can't keep it as varibale name
+
+        System.out.println(t);
+        System.out.println($);
+        System.out.println(_123);
+        System.out.println($rj);
+
     }
 }
