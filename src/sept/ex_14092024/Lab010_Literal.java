fcdd80e20ac3ae7ba6931525753ace396be6f42d
Tushar-W-QA <tusharwarad2929@gmail.com>
2024-09-17 09:49:11 +0530

    test

---
+++ src/sept/ex_14092024/Lab010_Literal.java
@@ -0,0 +1,8 @@
+package sept.ex_14092024;
+
+public class Lab010_Literal {
+    // dataType variable_name = variable_value (Literal)
+    // Type ogf Literal: Char, Integral, Boolean, Null Literal, Scientific / Exponent N Literal
+
+
+}
