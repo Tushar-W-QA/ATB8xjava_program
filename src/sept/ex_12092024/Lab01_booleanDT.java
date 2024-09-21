10322ca5e7c5d24489d185a3e9f219b8f3de3ada
Tushar-W-QA <tusharwarad2929@gmail.com>
2024-09-21 07:30:20 +0530

    assingment

--- src/sept/ex_12092024/Lab01_booleanDT.java
+++ src/sept/ex_12092024/Lab01_booleanDT.java
@@ -2,14 +2,14 @@ package sept.ex_12092024;
 
 public class Lab01_booleanDT {
     public static void main(String[] args) {
-        // datatype and variables (Java is caseSensitive language)
-        // primitive have two type datatype (
+        // datatype and variables (Java is case-sensitive language)
+        // primitive have two type datatype
         // 1. Boolean --> can have only true and false value (False or false both are different in java)
         // 2. Numeric --> 1. Integral  --> Byte, short, int, long, char
         //                2. Floating point --> float, double
 
-        // write code formate
-    //  datatype variable_name = variable_value
+        // write code format
+        //  datatype variable_name = variable_value
         boolean is_water_solid = false;
         boolean is_water_liquid = true;
         System.out.println(is_water_solid);
