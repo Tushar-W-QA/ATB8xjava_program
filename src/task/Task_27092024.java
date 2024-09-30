0aafa51aaace0502f30fef7b0dc469df7208a120
Tushar-W-QA <tusharwarad2929@gmail.com>
2024-09-30 07:05:07 +0530

    Task - 23092024

--- src/task/Task_27092024.java
+++ src/task/Task_27092024.java
@@ -1,6 +1,6 @@
 package task;
 
-public class Task_29092024 {
+public class Task_27092024 {
     public static void main(String[] args) {
 //        //  Create a Java program that prints the first 5 even numbers using a do-while loop.
 //        int count = 0;
